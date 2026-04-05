b9cdcad0e9d2a50a7cd7bc71fd25cf491f3486e4
the-shevos <samansda4@gmail.com>
2026-04-05 18:49:20 +0530

    Added Auth Service with DTO and Entity layers

---
+++ auth-service/src/main/java/com/example/auth_service/dto/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.example.auth_service.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private int status;
+    private String message;
+    private Object data;
+}
---
+++ auth-service/src/main/java/com/example/auth_service/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.auth_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
