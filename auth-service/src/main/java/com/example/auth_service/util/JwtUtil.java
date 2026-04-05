5c21793cc46ada455404fe43712fd4daade258f2
the-shevos <samansda4@gmail.com>
2026-04-05 18:58:04 +0530

    Added Auth Service utility classes

---
+++ auth-service/src/main/java/com/example/auth_service/util/JwtUtil.java
@@ -0,0 +1,71 @@
+package com.example.auth_service.util;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    private static final long ACCESS_TOKEN_VALIDITY = 1000 * 60 * 30; // 30 minutes
+    private static final long REFRESH_TOKEN_VALIDITY = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+    public String generateAccessToken(String username) {
+        return createToken(new HashMap<>(), username, ACCESS_TOKEN_VALIDITY);
+    }
+
+    public String generateRefreshToken(String username) {
+        return createToken(new HashMap<>(), username, REFRESH_TOKEN_VALIDITY);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject, long validity) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + validity))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getAllClaimsFromToken(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            return !getAllClaimsFromToken(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
